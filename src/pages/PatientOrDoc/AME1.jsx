00ac019a25b4e6d1613b5c6fdbe94cc657b7aa63
Sahil <dshiv867@gmail.com>
2024-06-30 21:10:58 +0530

    test data show in ame1 and pme

--- src/pages/PatientOrDoc/AME1.jsx
+++ src/pages/PatientOrDoc/AME1.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import CustomTable from '../../components/CustomTable'; // Adjust the import path as necessary
-import '../../styles/StylesP/HistoryData.css';
-import { Button } from '@mui/material';
-import Navbar from '../../components/Navbar';
-import '../../styles/StylesP/Ame.css';
-import { usePatientStore } from '../../stores/patientStore';
-import useAuth from '../../stores/authStore';
+import React, { useEffect, useState } from "react";
+import CustomTable from "../../components/CustomTable"; // Adjust the import path as necessary
+import "../../styles/StylesP/HistoryData.css";
+import { Button } from "@mui/material";
+import Navbar from "../../components/Navbar";
+import "../../styles/StylesP/Ame.css";
+import { usePatientStore } from "../../stores/patientStore";
+import useAuth from "../../stores/authStore";
 
 function AME1() {
   const { patient, testDate } = usePatientStore();
@@ -20,33 +20,50 @@ function AME1() {
       try {
         setIsLoading(true);
         const armyNo = patient?.armyNo; // Replace with the actual army number
-        const ame1Response = await makeAuthRequest('POST', `${API}/ame1testreports`, {
-          armyNo,
-          date: testDate, // Utilize medicalDate
-        });
-        setAME1Data(ame1Response?.data);
+        const ame1Response = await makeAuthRequest(
+          "POST",
+          `${API}/ame1testreports`,
+          {
+            armyNo,
+            date: testDate, // Utilize medicalDate
+          },
+        );
+        // console.log(ame1Response?.data);
+        const formattedData = {
+          "Blood HB": ame1Response?.data?.bloodHb,
+          TLC: ame1Response?.data?.TLC,
+          DLC: ame1Response?.data?.DLC,
+          "Blood Sugar Fasting": ame1Response?.data?.bloodSugarFasting,
+          "Blood Sugar Post Prandial": ame1Response?.data?.bloodSugarPP,
+          "Resting ECG": ame1Response?.data?.restingECG,
+          "Urine RE/ME": ame1Response?.data?.urineRE,
+          "Urine Sp Gravity": ame1Response?.data?.urineSpGravity,
+        };
+
+        setAME1Data(formattedData);
+        // setAME1Data(ame1Response?.data);
       } catch (error) {
         setIsLoading(false);
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
   }, []);
-  const ame1Heading = ['AME1', 'Data'];
+  const ame1Heading = ["AME1", "Data"];
 
   const ame1Rows = [
-    { ame1Info: 'BLOOD HB', data: 'MPQ134' },
+    { ame1Info: "BLOOD HB", data: "MPQ134" },
     {
-      ame1Info: 'TLC',
-      data: 'In this example, each row object contains two key-value pairs. The CustomTable component maps through these objects, ensuring the first value goes to the first column and the second value goes to the second column. This setup maintains flexibility while ensuring a consistent two-column structure.',
+      ame1Info: "TLC",
+      data: "In this example, each row object contains two key-value pairs. The CustomTable component maps through these objects, ensuring the first value goes to the first column and the second value goes to the second column. This setup maintains flexibility while ensuring a consistent two-column structure.",
     },
-    { ame1Info: 'DLC', data: 'Null' },
-    { ame1Info: 'URINE', data: 'M89' },
-    { ame1Info: 'URINESPGRAVITY', data: 'M89' },
-    { ame1Info: 'BLOODSUGARFASTING', data: 'M89' },
-    { ame1Info: 'BLOODSUGARPP', data: 'M89' },
-    { ame1Info: 'RESTINGECG', data: 'M89' },
+    { ame1Info: "DLC", data: "Null" },
+    { ame1Info: "URINE", data: "M89" },
+    { ame1Info: "URINESPGRAVITY", data: "M89" },
+    { ame1Info: "BLOODSUGARFASTING", data: "M89" },
+    { ame1Info: "BLOODSUGARPP", data: "M89" },
+    { ame1Info: "RESTINGECG", data: "M89" },
   ];
 
   const handlePrint = () => {
@@ -63,7 +80,7 @@ function AME1() {
         <button
           className="h-9 w-1/4 md:w-1/12 text-lg font-medium text-amber-400 border-2 border-[#efb034] mx-auto mb-5 rounded hover:bg-amber-400 hover:text-white hover:border-[#efb034]"
           onClick={handlePrint}
-          style={{ fontFamily: 'Manrope', fontOpticalSizing: 'auto' }}
+          style={{ fontFamily: "Manrope", fontOpticalSizing: "auto" }}
         >
           Print
         </button>
--- src/pages/PatientOrDoc/PME.jsx
+++ src/pages/PatientOrDoc/PME.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import CustomTable from '../../components/CustomTable'; // Adjust the import path as necessary
-import '../../styles/StylesP/HistoryData.css';
-import { Button } from '@mui/material';
-import Navbar from '../../components/Navbar';
-import '../../styles/StylesP/Ame.css';
-import { usePatientStore } from '../../stores/patientStore';
-import useAuth from '../../stores/authStore';
+import React, { useEffect, useState } from "react";
+import CustomTable from "../../components/CustomTable"; // Adjust the import path as necessary
+import "../../styles/StylesP/HistoryData.css";
+import { Button } from "@mui/material";
+import Navbar from "../../components/Navbar";
+import "../../styles/StylesP/Ame.css";
+import { usePatientStore } from "../../stores/patientStore";
+import useAuth from "../../stores/authStore";
 
 function PME() {
   const { patient, testDate } = usePatientStore();
@@ -21,45 +21,65 @@ function PME() {
       try {
         setIsLoading(true);
         const armyNo = patient?.armyNo; // Replace with the actual army number
-        const pmeResponse = await makeAuthRequest('POST', `${API}/pmetestreports`, {
-          armyNo,
-          date: testDate, // Utilize medicalDate
-        });
-        setPMEData(pmeResponse?.data);
+        const pmeResponse = await makeAuthRequest(
+          "POST",
+          `${API}/pmetestreports`,
+          {
+            armyNo,
+            date: testDate, // Utilize medicalDate
+          },
+        );
+        const formattedData = {
+          "Blood HB": pmeResponse?.data?.bloodHb,
+          TLC: pmeResponse?.data?.TLC,
+          DLC: pmeResponse?.data?.DLC,
+          "Blood Sugar Fasting": pmeResponse?.data?.bloodSugarFasting,
+          "Blood Sugar Post Prandial": pmeResponse?.data?.bloodSugarPP,
+          "Resting ECG": pmeResponse?.data?.restingECG,
+          Urea: pmeResponse?.data?.urea,
+          Creatinine: pmeResponse?.data?.creatinine,
+          Cholesterol: pmeResponse?.data?.cholesterol,
+          "Lipid Profile": pmeResponse?.data?.lipidProfile,
+          "Uric Acid": pmeResponse?.data?.uricAcid,
+          "Urine RE/ME": pmeResponse?.data?.urineRE,
+          "Urine Sp Gravity": pmeResponse?.data?.urineSpGravity,
+          "X-ray Chest PA": pmeResponse?.data?.xrayChestPA,
+        };
+        setPMEData(formattedData);
       } catch (error) {
         setIsLoading(false);
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
   }, []);
-  const PMEHeading = ['PME', 'Data'];
+  const PMEHeading = ["PME", "Data"];
 
   const PMERows = [
-    { PMEInfo: 'BLOOD HB', data: '13.5 g/dL' },
+    { PMEInfo: "BLOOD HB", data: "13.5 g/dL" },
     {
-      PMEInfo: 'TLC',
-      data: '6,000 cells/cu mm',
+      PMEInfo: "TLC",
+      data: "6,000 cells/cu mm",
     },
     {
-      PMEInfo: 'DLC',
-      data: 'Neutrophils: 60%, Lymphocytes: 30%, Monocytes: 5%, Eosinophils: 3%, Basophils: 2%',
+      PMEInfo: "DLC",
+      data: "Neutrophils: 60%, Lymphocytes: 30%, Monocytes: 5%, Eosinophils: 3%, Basophils: 2%",
     },
-    { PMEInfo: 'URINE', data: 'Normal' },
-    { PMEInfo: 'URINE SP. GRAVITY', data: '1.020' },
-    { PMEInfo: 'BLOOD SUGAR FASTING', data: '90 mg/dL' },
-    { PMEInfo: 'BLOOD SUGAR PP', data: '120 mg/dL' },
-    { PMEInfo: 'RESTING ECG', data: 'Normal sinus rhythm' },
-    { PMEInfo: 'URIC ACID', data: '5.5 mg/dL' },
-    { PMEInfo: 'UREA', data: '30 mg/dL' },
-    { PMEInfo: 'CREATININE', data: '1.0 mg/dL' },
-    { PMEInfo: 'CHOLESTEROL', data: '180 mg/dL' },
+    { PMEInfo: "URINE", data: "Normal" },
+    { PMEInfo: "URINE SP. GRAVITY", data: "1.020" },
+    { PMEInfo: "BLOOD SUGAR FASTING", data: "90 mg/dL" },
+    { PMEInfo: "BLOOD SUGAR PP", data: "120 mg/dL" },
+    { PMEInfo: "RESTING ECG", data: "Normal sinus rhythm" },
+    { PMEInfo: "URIC ACID", data: "5.5 mg/dL" },
+    { PMEInfo: "UREA", data: "30 mg/dL" },
+    { PMEInfo: "CREATININE", data: "1.0 mg/dL" },
+    { PMEInfo: "CHOLESTEROL", data: "180 mg/dL" },
     {
-      PMEInfo: 'LIPID PROFILE',
-      data: 'Total Cholesterol: 180 mg/dL, HDL: 50 mg/dL, LDL: 100 mg/dL, Triglycerides: 150 mg/dL',
+      PMEInfo: "LIPID PROFILE",
+      data: "Total Cholesterol: 180 mg/dL, HDL: 50 mg/dL, LDL: 100 mg/dL, Triglycerides: 150 mg/dL",
     },
-    { PMEInfo: 'X-RAY CHEST PA', data: 'Normal' },
+    { PMEInfo: "X-RAY CHEST PA", data: "Normal" },
   ];
 
   const handlePrint = () => {
@@ -76,7 +96,7 @@ function PME() {
         <button
           className="h-9 w-1/4 md:w-1/12 text-lg font-medium text-amber-400 border-2 border-[#efb034] mx-auto mb-5 rounded hover:bg-amber-400 hover:text-white hover:border-[#efb034]"
           onClick={handlePrint}
-          style={{ fontFamily: 'Manrope', fontOpticalSizing: 'auto' }}
+          style={{ fontFamily: "Manrope", fontOpticalSizing: "auto" }}
         >
           Print
         </button>
